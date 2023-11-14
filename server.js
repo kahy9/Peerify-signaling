696b4294c233cdc7c9d6079c516f3d1c8968ddac
Josef Kahoun <pepa.kahy@gmail.com>
2023-11-14 11:39:32 +0100

    Added new method of generating ids

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const { PeerServer } = require("peer");
+const { v4: uuidv4 } = require('uuid');
 
-const customGenerationFunction = () =>
-	(Math.random().toString(36) + "0000000000000000000").substr(2, 16);
+const customGenerationFunction = () => uuidv4();
 
 const peerServer = PeerServer({
-	port: 9000,
-	path: "/peerify",
-	generateClientId: customGenerationFunction,
+  port: 9000,
+  path: "/peerify",
+  generateClientId: customGenerationFunction,
 });
 
-peerServer.on('connection', (client) => (
-  console.log('client connected', client.id)
-));
+peerServer.on("connection", (client) =>
+  console.log("client connected", client.id)
+);
 
-peerServer.on('disconnect', (client) => (
-  console.log('client disconnected', client.id)
-));
+peerServer.on("disconnect", (client) =>
+  console.log("client disconnected", client.id)
+);
